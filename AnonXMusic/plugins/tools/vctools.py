5151adc2cebd98038b8e2c2b583351397fbdd7f0
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-02-05 13:20:06 +0530

    Update vctools.py

--- AnonXMusic/plugins/tools/vctools.py
+++ AnonXMusic/plugins/tools/vctools.py
@@ -28,7 +28,7 @@ async def brah3(app: app, message: Message):
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
-            text += f"{message.from_user.mention}"
+            text += f"➻ {message.from_user.mention} \n\n "
             x += 1
         except Exception:
             pass
